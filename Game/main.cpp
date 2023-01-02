dfae0cef9a7f5630b96acf5240a4d0d31af70bc7
mr3e <93253016+mr3e@users.noreply.github.com>
2023-01-02 22:17:57 +0200

    Finished Assignment

--- Game/main.cpp
+++ Game/main.cpp
@@ -1,33 +1,32 @@
+#include <glfw/deps/glad/gl.h>
 #include "InputManager.h"
-// #include "../DisplayGLFW/display.h"
+#include "display.h"
 #include "game.h"
-#include "../res/includes/glm/glm.hpp"
+#include "glm\glm.hpp"
+#include "../Engine3D/stb_image.h"
+#include <stdio.h>
+#include <iostream>
+#include <glfw/deps/linmath.h>
 
-int main(int argc,char *argv[])
+int main(int argc, char* argv[])
 {
 	const int DISPLAY_WIDTH = 800;
 	const int DISPLAY_HEIGHT = 800;
-	const float CAMERA_ANGLE = 0.0f;
+	const float CAMERA_ANGLE = 45.0f;
 	const float NEAR = 1.0f;
 	const float FAR = 100.0f;
 
-	Game *scn = new Game(CAMERA_ANGLE,(float)DISPLAY_WIDTH/DISPLAY_HEIGHT,NEAR,FAR);
-	
-	Display display(DISPLAY_WIDTH, DISPLAY_HEIGHT, "OpenGL");
-	
-	Init(display);
-	
+	Game* scn = new Game(CAMERA_ANGLE, (float)DISPLAY_WIDTH / DISPLAY_HEIGHT, NEAR, FAR);
+	Display display(DISPLAY_WIDTH, DISPLAY_HEIGHT, "Assignment 3!");
+
 	scn->Init();
-
+	Init(display);
 	display.SetScene(scn);
-
-	while(!display.CloseWindow())
-	{
-		scn->Draw(1,0,scn->BACK,true,false);
+	while (!display.CloseWindow()) {
+		scn->Draw(1, 0, scn->BACK, true, false);
 		scn->Motion();
 		display.SwapBuffers();
-		display.PollEvents();	
-			
+		display.PollEvents();
 	}
 	delete scn;
 	return 0;
